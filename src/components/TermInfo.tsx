0c4172b55268d02f6d1e9e3b9c6fc8c02bc0df50
Amin <23167933+aminsaedi@users.noreply.github.com>
2025-01-24 13:16:37 -0500

    Update TermInfo.tsx

--- src/components/TermInfo.tsx
+++ src/components/TermInfo.tsx
@@ -3,7 +3,7 @@ import { User, WebsiteName, Wrapper } from "./styles/TerminalInfo.styled";
 const TermInfo = () => {
   return (
     <Wrapper>
-      <User>visitor</User>@<WebsiteName>aminsaedi.bio</WebsiteName>:~$
+      <User>visitor</User>@<WebsiteName>saedi.site</WebsiteName>:~$
     </Wrapper>
   );
 };
